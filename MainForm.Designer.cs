d0da136287b29040cd7fb3e36ff7e0d0a0a6c361
Faith Mhlanga <ST10465004@rcconnect.edu.za>
2026-05-25 10:11:24 +0200

    Designed Windows Forms Chatbot interface layout.
    Designed a simple Windows Forms chatbot interface layout with a button, textbox and label.  Changed main colour (cyan) from part 1 to another - Pink.

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -28,20 +28,76 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.rtbChat = new System.Windows.Forms.RichTextBox();
+            this.txtUserInput = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // Form1
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Times New Roman", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.ForeColor = System.Drawing.Color.Pink;
+            this.lblTitle.Location = new System.Drawing.Point(192, 25);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(470, 39);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "Cybersecurity Awareness Chatbot";
+            // 
+            // rtbChat
+            // 
+            this.rtbChat.BackColor = System.Drawing.Color.Black;
+            this.rtbChat.ForeColor = System.Drawing.Color.White;
+            this.rtbChat.Location = new System.Drawing.Point(40, 83);
+            this.rtbChat.Name = "rtbChat";
+            this.rtbChat.ReadOnly = true;
+            this.rtbChat.Size = new System.Drawing.Size(800, 350);
+            this.rtbChat.TabIndex = 1;
+            this.rtbChat.Text = "";
+            // 
+            // txtUserInput
+            // 
+            this.txtUserInput.Location = new System.Drawing.Point(40, 465);
+            this.txtUserInput.Name = "txtUserInput";
+            this.txtUserInput.Size = new System.Drawing.Size(650, 22);
+            this.txtUserInput.TabIndex = 2;
+            // 
+            // btnSend
+            // 
+            this.btnSend.BackColor = System.Drawing.Color.LightPink;
+            this.btnSend.ForeColor = System.Drawing.Color.Black;
+            this.btnSend.Location = new System.Drawing.Point(712, 459);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(100, 35);
+            this.btnSend.TabIndex = 3;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = false;
+            // 
+            // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Name = "Form1";
-            this.Text = "Form 1";
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(882, 553);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.txtUserInput);
+            this.Controls.Add(this.rtbChat);
+            this.Controls.Add(this.lblTitle);
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Cybersecurity Awareness Chatbot";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.RichTextBox rtbChat;
+        private System.Windows.Forms.TextBox txtUserInput;
+        private System.Windows.Forms.Button btnSend;
     }
 }
 
