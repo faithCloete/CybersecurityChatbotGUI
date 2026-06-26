e923f52974a26769d30b967ea4b8000f58f2d581
Faith Mhlanga <ST10465004@rcconnect.edu.za>
2026-06-26 10:55:59 +0200

    Database connection helper.
    Created database connection helper class

---
+++ DatabaseHelper.cs
@@ -0,0 +1,17 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace CybersecurityChatbotGUI
+{
+    public class DatabaseHelper
+    {
+        //connection string
+        private string connectionString =
+            "server=localhost;database=CybersecurityChatbotDB;uid=root;pwd=F@ithrosemhlangacloete62;";
+
+        //method to get database connection
+        public MySqlConnection GetConnection()
+        {
+            return new MySqlConnection(connectionString);
+        }
+    }
+}
